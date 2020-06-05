9fd77e0b0bc7d2b7f1197addafe76ce3365c7f59
AliAbdulle <alidhobey@gmail.com>
2020-06-04 19:39:04 -0500

    saved after fixed the error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './components/Header'
 
 
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header />
       <h3> Hello Cart</h3>
     </div>
   );
