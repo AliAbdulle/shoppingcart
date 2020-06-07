6d85a02916ec198f25ec80ecb664b94f982e1b2b
AliAbdulle <alidhobey@gmail.com>
2020-06-06 22:32:26 -0500

    indexkey fetch the data

--- src/components/pages/Photos.js
+++ src/components/pages/Photos.js
@@ -3,7 +3,9 @@ import React, {useContext} from 'react'
 function Photos() {
     const {allPhotoes} =useContext(Context)
 
-    const imageElements = allPhotoes((img, 1) => )
+    const imageElements = allPhotoes((img, 1) => (
+        <Image key={img.id} img={img} className={getClasse(<index of image>)} />
+    ))
     return (
         <main className="cart-page">
             <h1>Check out</h1>
