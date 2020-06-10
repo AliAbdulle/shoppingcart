fbbf6d0332c3560b119b22d5ada79057e425a532
AliAbdulle <alidhobey@gmail.com>
2020-06-09 20:30:33 -0500

    Image source gride added

---
+++ src/components/Image.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Image() {
+    return (
+        <div className="className">
+            <img src={???} className="image-gride" />
+        </div>
+    )
+}
+
+export default Image
