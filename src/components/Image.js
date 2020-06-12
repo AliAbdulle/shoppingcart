23b171dcc2a33f1bd183b1e69416e7217b369d92
AliAbdulle <alidhobey@gmail.com>
2020-06-11 22:58:02 -0500

    conditional randering

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,13 +2,17 @@ import React, {useState} from "react"
 
 function Image({className, img}) {
     const [hovered, setHovered] = useState(false)
-    console.log(hovered)
+    
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const carttIcon = hovered && <i className="ri-add-circle-line cart"></i>
     return (
         <div className={`${className} image-container`}
         onMouseEnter={() => setHovered(true)}
         oneMouseLeave ={() => setHovered(false)}
         >
             <img src={img.url} className="image-grid"/>
+            {heartIcon}
+            {carttIcon}
         </div>
     )
 }
